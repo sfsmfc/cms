80e5193a24db0ebe7dfdcaf0500cb818076a8b00
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-10-28 16:34:08 -0400

    New translations app.php (Hungarian) [ci skip]

--- src/translations/hu/app.php
+++ src/translations/hu/app.php
@@ -93,6 +93,7 @@ return [
     'All users' => 'Az összes felhasználó',
     'All' => 'Mind',
     'Allow Upscaling' => 'Upscaling engedélyezése',
+    'Allow a custom option' => 'Allow a custom option',
     'Allow anchors' => 'Allow anchors',
     'Allow line breaks' => 'Sortörés engedélyezése',
     'Allow public registration' => 'Engedélyezed a publikus regisztrációt',
