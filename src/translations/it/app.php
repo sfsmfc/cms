00cb0b57e6ca55f970f944f24e74d68a66894ea6
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-10-30 18:06:50 -0400

    New translations app.php (Italian) [ci skip]

--- src/translations/it/app.php
+++ src/translations/it/app.php
@@ -1454,6 +1454,8 @@ return [
     'Show the debug toolbar in the control panel' => 'Mostra la barra degli strumenti per il debug nel pannello di controllo',
     'Show the debug toolbar on the front end' => 'Mostra la barra degli strumenti per il debug in primo piano',
     'Show the site menu' => 'Mostra il menu del sito',
+    'Show the “Label” field' => 'Show the “Label” field',
+    'Show the “Open in a new tab” field' => 'Show the “Open in a new tab” field',
     'Show them indefinitely' => 'Mostrale senza limiti di tempo',
     'Show thumbnails and titles' => 'Mostra miniature e titoli',
     'Show thumbnails only' => 'Mostra solo le miniature',
