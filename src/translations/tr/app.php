393e0abad5320f1db1f46eeaf4421d932d301f7e
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-10-30 18:07:05 -0400

    New translations app.php (Turkish) [ci skip]

--- src/translations/tr/app.php
+++ src/translations/tr/app.php
@@ -1454,6 +1454,8 @@ return [
     'Show the debug toolbar in the control panel' => 'Hata ayıklama araç çubuğunu kontrol panelinde göster',
     'Show the debug toolbar on the front end' => 'Hata ayıklama araç çubuğunu ön uçta göster',
     'Show the site menu' => 'Site menüsünde göster',
+    'Show the “Label” field' => 'Show the “Label” field',
+    'Show the “Open in a new tab” field' => 'Show the “Open in a new tab” field',
     'Show them indefinitely' => 'Süresiz olarak göster',
     'Show thumbnails and titles' => 'Kapak resimlerini ve başlıkları göster',
     'Show thumbnails only' => 'Sadece kapak resimlerini göster',
