09ccce803a9098c242e8efd8f3c2504556ab9352
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-12-14 12:54:13 -0500

    New translations app.php (Persian) [ci skip]

--- src/translations/fa/app.php
+++ src/translations/fa/app.php
@@ -442,7 +442,6 @@ return [
     'Current Password' => 'رمز عبور فعلی',
     'Current User Condition' => 'شرط کاربر فعلی',
     'Current' => 'فعلی',
-    'Currently SVG images do not support focal point.' => 'در حال حاضر تصاویر SVG از نقطه کانونی پشتیبانی نمی کند.',
     'Custom' => 'شخصی‌سازی',
     'Customize sources' => 'شخصی‌سازی منابع',
     'Custom…' => 'شخصی سازی...',
