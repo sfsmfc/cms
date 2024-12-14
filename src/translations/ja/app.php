79022b4acac4274a89818b52b27e2221861c5f67
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-12-14 12:54:55 -0500

    New translations app.php (Japanese) [ci skip]

--- src/translations/ja/app.php
+++ src/translations/ja/app.php
@@ -469,7 +469,6 @@ return [
     'Current Password' => '現在のパスワード',
     'Current User Condition' => '現在のユーザーの条件',
     'Current' => '現在',
-    'Currently SVG images do not support focal point.' => '現在、SVG 画像では焦点をサポートしていません。',
     'Custom Fields' => 'カスタムフィールド',
     'Custom' => 'カスタム',
     'Customize sources' => 'ソースのカスタマイズ',
