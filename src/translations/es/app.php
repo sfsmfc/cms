3a6228e667d01058da8fb4e796c1b8ed7b7b1174
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-12-14 12:54:45 -0500

    New translations app.php (Spanish) [ci skip]

--- src/translations/es/app.php
+++ src/translations/es/app.php
@@ -469,7 +469,6 @@ return [
     'Current Password' => 'Contraseña vigente',
     'Current User Condition' => 'Condición de usuario actual',
     'Current' => 'Actual',
-    'Currently SVG images do not support focal point.' => 'Actualmente, las imágenes SVG no son compatibles con el punto focal.',
     'Custom Fields' => 'Campos personalizados',
     'Custom' => 'Personalizado',
     'Customize sources' => 'Fuentes personalizadas',
