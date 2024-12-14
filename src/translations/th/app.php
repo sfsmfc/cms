e858ab0a552963e17112150f2b007300ecbbd708
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-12-14 12:54:15 -0500

    New translations app.php (Thai) [ci skip]

--- src/translations/th/app.php
+++ src/translations/th/app.php
@@ -442,7 +442,6 @@ return [
     'Current Password' => 'Current Password',
     'Current User Condition' => 'เงื่อนไขผู้ใช้ปัจจุบัน',
     'Current' => 'Current',
-    'Currently SVG images do not support focal point.' => 'Currently SVG images do not support focal point.',
     'Custom' => 'กำหนดเอง',
     'Customize sources' => 'ปรับแต่งแหล่งที่มา',
     'Custom…' => 'Custom…',
